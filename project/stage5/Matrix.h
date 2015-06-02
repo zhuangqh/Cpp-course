f2ab0bc1b9d43505d2b7a11f0d4fd10b1f57a1f1
zhuangqh <zhuangqhc@163.com>
2015-06-03 00:26:33 +0800

    beautiful code

---
+++ project/stage5/Matrix.h
@@ -0,0 +1,239 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+#include "Base.h"
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <assert.h>
+#include <stdexcept>
+template<typename T>
+class Matrix : public Base<T> {
+ private:
+  T deter;
+  //basic matrix operation
+  int find_pivot_from(int);
+  void swap_row(int , int);
+  void replace(int dest, int src, T constant);
+ public:
+  Matrix() : Base<T>() {}
+  Matrix(const Matrix<T> &other) : Base<T>(other) {}
+  Matrix(int row, int col) : Base<T>(row, col) {}
+  ~Matrix() {}
+  Matrix row(int) const;
+  Matrix col(int) const;
+  T max_entry() const;
+  T min_entry() const;
+  Matrix transpose() const;
+  T determinant();
+  Matrix inverse();
+  void hear(const std::string&, const Matrix<T>&);
+  Matrix operator+(const Matrix<T> &) const;
+  Matrix operator-(const Matrix<T> &) const;
+  Matrix operator*(const Matrix<T> &) const;
+  template <typename S>
+    friend std::ostream& operator<<(std::ostream&, const Matrix<S>&);
+};
+
+template <typename T>
+int Matrix<T>::find_pivot_from(int index) {
+  for (int i = index; i < Base<T>::_row; ++i) {
+    if (Base<T>::_data[i][index]) {
+      deter *= -1;
+      return i;
+    }
+  }
+  return -1;
+}
+
+template <typename T>
+void Matrix<T>::swap_row(int row1, int row2) {
+  if (row1 == row2) return;
+  for (int i = 0; i < Base<T>::_col; ++i) {
+    T temp = Base<T>::_data[row1][i];
+    Base<T>::_data[row1][i] = Base<T>::_data[row2][i];
+    Base<T>::_data[row2][i] = temp;
+  }
+}
+
+template<typename T>
+void Matrix<T>::replace(int dest, int src, T constant) {
+  for (int i = 0; i < Base<T>::_col; ++i) {
+    Base<T>::_data[dest][i] -= constant * Base<T>::_data[src][i];
+  }
+}
+
+//print the matrix with a good layout
+template<typename T>
+std::ostream& operator<<(std::ostream& output, const Matrix<T>& m) {
+  for (int i = 0; i < m._row; ++i) {
+    output << "| ";
+    for (int j = 0; j < m._col; ++j) {
+      output << std::setw(6) << std::setfill(' ') << m._data[i][j] << " ";
+    }
+    output << " |" << std::endl;
+  }
+  return output;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::row(int row_num) const{
+  Matrix<T> ans(1, Base<T>::_col);
+  for (int i = 0; i < Base<T>::_col; ++i)
+    ans._data[0][i] = Base<T>::_data[row_num][i];
+  return ans;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::col(int col_num) const{
+  Matrix<T> ans(Base<T>::_row, 1);
+  for (int i = 0; i < Base<T>::_row; ++i)
+    ans._data[i][0] = Base<T>::_data[i][col_num];
+  return ans;
+}
+
+template<typename T>
+T Matrix<T>::max_entry() const{
+  T max_entry = Base<T>::_data[0][0];
+  for (int i = 0; i < Base<T>::_row; ++i) {
+    for (int j = 0; j < Base<T>::_col; ++j) {
+      max_entry = std::max(max_entry, Base<T>::_data[i][j]);
+    }
+  }
+  return max_entry;
+}
+
+template<typename T>
+T Matrix<T>::min_entry() const {
+  T min_entry = Base<T>::_data[0][0];
+  for (int i = 0; i < Base<T>::_row; ++i) {
+    for (int j = 0; j < Base<T>::_col; ++j) {
+      min_entry = std::min(min_entry, Base<T>::_data[i][j]);
+    }
+  }
+  return min_entry;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::transpose() const {
+  Matrix<T> ans(Base<T>::_col, Base<T>::_row);
+  for (int i = 0; i < Base<T>::_col; ++i) {
+    for (int j = 0; j < Base<T>::_row; ++j) {
+      ans._data[i][j] = Base<T>::_data[j][i];
+    }
+  }
+  return ans;
+}
+
+template <typename T>
+T Matrix<T>::determinant() {
+  Matrix<T> ans(*this);
+  ans.deter = 1;
+  //reduce the matrix to its echelon form
+  for (int i = 0; i < ans._row - 1; ++i) {
+    int pivot = find_pivot_from(i);
+    assert(pivot >= 0);
+    swap_row(i, pivot);
+    for (int j = i + 1; j < ans._row; ++j) {
+      ans.replace(j, i, ans._data[j][i] / ans._data[i][i]);
+    }
+  }
+  //calculate the determinant
+  for (int i = 0; i < ans._row; ++i) {
+    ans.deter *= ans._data[i][i];
+  }
+  deter = ans.deter;
+  return deter;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::inverse() {
+  if (Base<T>::_col != Base<T>::_row)
+    throw std::logic_error("The matrix is not inversible!");
+  Matrix<T> ans(Base<T>::_row, Base<T>::_col);
+  Matrix<T> src(*this);
+  //create a identity matrix
+  for (int i = 0; i < ans._row; ++i)
+    ans._data[i][i] = 1;
+  //using Gaussian Elimination to get the inverse of matrix
+  //1. reduce the matrix 'src' to echelon form
+  for (int i = 0; i < src._row; ++i) {
+    int pivot = src.find_pivot_from(i);
+    if (pivot < 0)
+      throw std::logic_error("The matrix is not inversible!");
+
+    src.swap_row(i, pivot);
+    ans.swap_row(i, pivot);
+    for (int j = i + 1; j < ans._row; ++j) {
+      T constant = src._data[j][i] / src._data[i][i];
+      src.replace(j, i, constant);
+      ans.replace(j, i, constant);
+    }
+  }
+  //2. reduce the matrix 'src' to identity form
+  for (int i = Base<T>::_row - 1; i >= 0; --i) {
+    for (int j = i; j >= 0; --j) {
+      if (j == i) {
+        for (int k = 0; k < ans._col; ++k) {
+          ans._data[i][k] /= src._data[i][i];
+          src._data[i][i] = 1;
+        }
+      } else {
+        T constant = src._data[j][i] / src._data[i][i];
+        src.replace(j, i, constant);
+        ans.replace(j, i, constant);
+      }
+    }
+  }
+  return ans;
+}
+
+template <typename T>
+void Matrix<T>::hear(const std::string &str, const Matrix<T>& m) {
+  if (str == "I love") *this = *this + m;
+  if (str == "I hate") *this = *this - m;
+}
+template <typename T>
+Matrix<T> Matrix<T>::operator+(const Matrix<T> &other) const{
+  if ((Base<T>::_row != other._row) || (Base<T>::_col != other._col))
+    throw std::invalid_argument("The size of matrix is not matched.");
+
+  Matrix<T> ans(Base<T>::_row, Base<T>::_col);
+  for (int i = 0; i < Base<T>::_row; ++i) {
+    for (int j = 0; j < Base<T>::_col; ++j) {
+      ans._data[i][j] = Base<T>::_data[i][j] + other._data[i][j];
+    }
+  }
+  return ans;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator-(const Matrix<T> &other) const{
+  if ((Base<T>::_row != other._row) || (Base<T>::_col != other._col))
+    throw std::invalid_argument("The size of matrix is not matched.");
+
+  Matrix ans(Base<T>::_row, Base<T>::_col);
+  for (int i = 0; i < Base<T>::_row; ++i) {
+    for (int j = 0; j < Base<T>::_col; ++j) {
+      ans._data[i][j] = Base<T>::_data[i][j] - other._data[i][j];
+    }
+  }
+  return ans;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T> &other) const{
+  if ((Base<T>::_row != other._col) || (Base<T>::_col != other._row))
+    throw std::invalid_argument("The size of matrix is not matched.");
+
+  Matrix<T> ans(Base<T>::_row, other._col);
+  for (int i = 0; i < Base<T>::_row; ++i) {
+    for (int j = 0; j < other._col; ++j) {
+      for (int k = 0; k < Base<T>::_col; ++k) {
+        ans._data[i][j] += Base<T>::_data[i][k] * other._data[k][j];
+      }
+    }
+  }
+  return ans;
+}
+
+#endif
---
+++ project/stage5/main.cpp
@@ -0,0 +1,220 @@
+#include "Matrix.h"
+#include "Vector.h"
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+void BaseMethodOfMatrix();
+void BaseMethodOfVector();
+void MatrixAdvancedMethod();
+void StreamOverloading();
+void ShowExceptionHandle();
+void TestFileInputOutputAndHandle();
+int main() {
+  std::cout << "Which part would you like to test?\n";
+  std::cout << "0------The base methods of Matrix\n";
+  std::cout << "1------The base methods of Vector\n";
+  std::cout << "2------Matrix's advanced methods\n";
+  std::cout << "3------Stream Overloading test\n";
+  std::cout << "4------Show the Exception Handle\n";
+  std::cout << "5------Test file input/output and handle\n";
+  int mode;
+  std::cin >> mode;
+  try {
+    switch(mode) {
+    case 0: BaseMethodOfMatrix(); break;
+    case 1: BaseMethodOfVector(); break;
+    case 2: MatrixAdvancedMethod(); break;
+    case 3: StreamOverloading(); break;
+    case 4: ShowExceptionHandle(); break;
+    case 5: TestFileInputOutputAndHandle(); break;
+    }
+  } catch (const out_of_range& e) {
+    cout << "Exception occurred : "
+         << e.what() << endl
+         << "Error Type : " << typeid(e).name() << endl;
+  } catch (const invalid_argument& e) {
+    cout << "Exception occurred : "
+         << e.what() << endl
+         << "Error Type : " << typeid(e).name() << endl;
+  } catch (const logic_error& e) {
+    cout << "Exception occurred : "
+         << e.what() << endl
+         << "Error Type : " << typeid(e).name() << endl;
+  }
+  return 0;
+}
+
+void BaseMethodOfMatrix() {
+  //show the method of class Matrix
+  cout << "**************************\n\n"
+       << "The base methods of Matrix\n\n"
+       << "**************************\n";
+  const double C_2D_array[3][3] = {
+    { 1.1, 2.3, 3.3 },
+    { 4.5, 5.7, 6.8 },
+    { 7.8, 8.2, 9.3 }
+  };
+  Matrix<double> example;
+  //method #1
+  cout << "example is " << endl;
+  example.set(C_2D_array);
+  cout << example << endl;
+  //method #2
+  cout << "example is " << endl;
+  example.set_one(0, 0, 0);
+  cout << example << endl
+       << "after set index[0][0] to 0" << endl;
+  //method #4
+  int *row_col_message = example.size();
+  cout << "The row of example is " << row_col_message[0] << endl
+       << "The column of example is " << row_col_message[1] << endl;
+  //free the memory
+  delete row_col_message;
+  //method #5
+  cout << "Entry of row 1 col 1 is " << example.get(1, 1) << endl;
+  //method #6
+  cout << "row 1 is " << endl
+       << example.row(1) << endl;
+  //method #7
+  cout << "col 1 is " << endl
+       << example.col(1) << endl;
+  //method #8
+  cout << "The maxinum entry of example is "
+       << example.max_entry() << endl;
+  //method #9
+  cout << "The mininum entry of example is "
+       << example.min_entry() << endl;
+  //method #10
+  cout << "example + example =" << endl;
+  Matrix<double> sum = example + example;
+  cout << sum << endl;
+  //method #11
+  cout << "example - example =" << endl;
+  Matrix<double> substract = example - example;
+  cout << substract << endl;
+  //method #12
+  cout << "example * example =" << endl;
+  Matrix<double> multiply = example * example;
+  cout << multiply << endl;
+}
+
+void BaseMethodOfVector() {
+  //show the method of class Vector;
+  cout << "\n**************************\n\n"
+       << "The base methods of Vector\n\n"
+       << "**************************\n";
+  Vector<int> vec;
+  const int C_2D_array2[3][1] = { 1, 2, 3 };
+  //method #1
+  cout << "vec is ";
+  vec.set(C_2D_array2);
+  cout << vec << endl;
+  //mrthod #2
+  cout << "vec is ";
+  vec.set_one(0, 0);
+  cout << vec << endl;
+  cout << "after set index 0 to 0" << endl;
+  //method #3
+  cout << "The dimension of vec is " << vec.dimension() << endl;
+  //method #4
+  cout << "The length of vec is " << vec.length() << endl;
+  //method #5
+  cout << "vec + vec = ";
+  Vector<int> sum2 = vec + vec;
+  cout << sum2 << endl;
+  //method #6
+  cout << "vec - vec = ";
+  Vector<int> substract2 = vec - vec;
+  cout << substract2 << endl;
+  //method #7
+  cout << "vec * vec = ";
+  Vector<int> multiply2(vec * vec);
+  cout << multiply2 << endl;
+
+  //show the new method "all" and "any"
+  Matrix<int> base;
+  base.set(C_2D_array2);
+  //method "all"
+  int check_for_all[3] = { 1, 2, 3 };
+  cout << (base.all(check_for_all) ? "True\n" : "False\n");
+  //method "any"
+  int check_for_any[3] = { 11, 22, 33 };
+  cout << (base.any(check_for_any) ? "True\n" : "False\n");
+}
+
+void MatrixAdvancedMethod() {
+    //show the new method "determinant", "transpose" and "inverse"
+  cout << "*************************\n\n"
+       << "Matrix's advanced methods\n\n"
+       << "*************************\n";
+  double C_2D_array3[5][5] = {{1, 0, 1, 1, 1},
+                              {0, 1, 0, 0, 0},
+                              {2, 0, 3, 5, 2},
+                              {0, 4, 0, 1, 4},
+                              {0, 2, 0, 0, 1}};
+  Matrix<double> example2;
+  example2.set(C_2D_array3);
+  cout << "Matrix example2 is " << endl
+       << example2 << endl;
+  //show the method "inverse"
+  cout << "The inverse of 'example2' is " << endl
+       << example2.inverse() << endl;
+  cout << endl << "example2 * example2-1 is " << endl
+       << example2 * example2.inverse() << endl;
+  //show the method "determinant"
+  cout << "The determinant of example2 is "
+       << example2.determinant() << endl;
+  //show the method "transpose"
+  cout << "The transpose form of example2 is " << endl
+       << example2.transpose() << endl;
+  //show the overload of operator ()
+  cout << "example2(2, 2) is " << example2(2, 2) << endl;
+}
+
+void StreamOverloading() {
+  cout << "******************\n"
+       << "Stream Overloading\n"
+       << "******************\n";
+  Matrix<double> o1;
+  o1 << "1.1,2.2,3.3;4.4,5,6;7,8,9";
+  cout << o1 << endl;
+  Vector<double> o2;
+  o2 << "1.2,2.3,3.4";
+  cout << o2 << endl;
+}
+
+void ShowExceptionHandle() {
+  cout << "*************************\n"
+       << "Show the exception handle\n"
+       << "*************************\n";
+    double value[2][2] = {{1, 2},
+                          {2, 4}};
+    double value2[1][2] = {1, 2};
+    Matrix<double> m1, m2;
+    m1.set(value);
+    m2.set(value2);
+    //Please comment or uncomment some of the statement
+    //to test the exception handle  :)
+    cout << "m1 + m2 = " << m1 + m2 << endl;
+    cout << "inverse of m1 " << m1.inverse() << endl;
+    //cout << m1(2, 2) << endl;
+
+}
+
+void TestFileInputOutputAndHandle() {
+  Matrix<double> example3, example4;
+  example3.setByFile("/home/zhuangqh/Cpp_homework/project/stage5/matrix1.txt",
+                     3, 3);
+  example4.setByFile("/home/zhuangqh/Cpp_homework/project/stage5/matrix2.txt",
+                     3, 3);
+  std::cout << example3 << std::endl << example4 << std::endl;
+  example3.sort("descending");
+  std::cout << "After sort \"descending\"\n" << example3 << std::endl;
+  example3.printToFile("/home/zhuangqh/Cpp_homework/project/stage5/matrixout.txt");
+  std::cout << "example3 hate example4\n";
+  example3.hear("I hate", example4);
+  std::cout << example3 << std::endl;
+  std::cout << "example3 love example4\n";
+  example3.hear("I love", example4);
+  std::cout << example3 << std::endl;
+}
